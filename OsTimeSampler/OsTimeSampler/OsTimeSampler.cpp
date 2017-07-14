c1cfd3a9cfc87ca7065234f1f5fadadac89cdfc9
Matthew Kennedy <t-maken@microsoft.com>
2017-07-14 12:30:15 -0700

    Add multiple measurements to OsTimeSampler

--- OsTimeSampler/OsTimeSampler/OsTimeSampler.cpp
+++ OsTimeSampler/OsTimeSampler/OsTimeSampler.cpp
@@ -25,26 +25,50 @@ int main(int argc, char ** argv)
     DWORD64 tscStart;
     DWORD64 tscEnd;
     FILETIME ft;
+	LARGE_INTEGER pc;
     size_t interations;
     DWORD interval;
+	DWORD method;
 
-    if (argc != 3) {
-        printf("Usage: %s interval count\n", argv[0]);
+    if (argc != 4) {
+        printf("Usage: %s method interval count\n", argv[0]);
         return -1;
     }
 
     printf("TSC_START, TSC_END, SYSTEM_TIME, TIME_ADJ, TIME_INC, TIME_ADJ_ACTIVE\n");
-    interval = atoi(argv[1]);
-    interations = atoi(argv[2]);
+	method = atoi(argv[1]);
+    interval = atoi(argv[2]);
+    interations = atoi(argv[3]);
 
     for (size_t i = 0; i < interations; i++) {
         DWORD timeAdjustment = 0;
         DWORD timeIncrement = 0;
         BOOL timeAdjEnabled = FALSE;
         Sleep(interval);
-        tscStart = __rdtsc();
-        GetSystemTimePreciseAsFileTime(&ft);
-        tscEnd = __rdtsc();
+
+		switch (method) {
+		case 0:
+			tscStart = __rdtsc();
+			GetSystemTimeAsFileTime(&ft);
+			tscEnd = __rdtsc();
+			break;
+		case 1:
+			tscStart = __rdtsc();
+			GetSystemTimePreciseAsFileTime(&ft);
+			tscEnd = __rdtsc();
+			break;
+		case 2:
+			tscStart = __rdtsc();
+			QueryPerformanceCounter(&pc);
+			tscEnd = __rdtsc();
+
+			// dump in to a filetime
+			ft.dwHighDateTime = pc.HighPart;
+			ft.dwLowDateTime = pc.LowPart;
+			
+			break;
+		}
+
         GetSystemTimeAdjustment(&timeAdjustment, &timeIncrement, &timeAdjEnabled);
 
         printf("%lld, %lld, %lld, %d, %d, %s\n", tscStart, tscEnd, *(DWORD64*)&ft, timeAdjustment, timeIncrement, !timeAdjEnabled ? "true" : "false");
